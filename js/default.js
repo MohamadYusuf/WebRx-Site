6bd00baa8ea185845cb54de20be2f11b7f6e9cec
Oliver Weichhold <oliver@weichhold.com>
2015-03-18 21:20:39 +0100

    Misc

--- js/default.js
+++ js/default.js
@@ -1,8 +1,7 @@
-$( document ).ready(function() {
-  // hide impressum for international users
-  if(navigator.language != 'de' && navigator.language.indexOf('de-') != 0)
-  	$('#impressum').remove();
-
-  // inject linkedin placeholder
-  $("#linkedin-placeholder").html('<script src="//platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script><script type="IN/Share" data-url="http://eonmatrix.com" data-counter="top"></script>');
-});
+// hide impressum for international users
+if(navigator.language != 'de' && navigator.language.indexOf('de-') != 0) {
+	var el = document.querySelector('#impressum');
+	if(el) {
+		el.parentNode.removeChild(el);
+	}
+}
