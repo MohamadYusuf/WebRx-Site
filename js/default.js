480e19eda96fb246fc3f09c5d8c804356fdeb99d
Oliver Weichhold <oliver@weichhold.com>
2015-03-19 21:28:11 +0100

    Live demo

--- js/default.js
+++ js/default.js
@@ -5,3 +5,19 @@ if(navigator.language != 'de' && navigator.language.indexOf('de-') != 0) {
 		el.parentNode.removeChild(el);
 	}
 }
+
+if(document.location.pathname === '/') {
+    function TicketsViewModel() {
+        this.tickets = [
+            { name: "Economy", price: 199.95 },
+            { name: "Business", price: 449.22 },
+            { name: "First Class", price: 1199.99 }
+        ];
+        this.chosenTicket = wx.property();
+        this.resetTicket = function() { 
+        	this.chosenTicket(null);
+        }
+    }
+
+	wx.applyBindings(new TicketsViewModel(), document.getElementById("live-demo"));
+}
