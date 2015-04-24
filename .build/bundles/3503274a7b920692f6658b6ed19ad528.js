93bdea4d35b8a72ccab9b6d262caad6bd12343a6
Oliver Weichhold <oliver@weichhold.com>
2015-04-24 21:46:39 +0200

    Script upodate

--- .build/bundles/3503274a7b920692f6658b6ed19ad528.js
+++ .build/bundles/3503274a7b920692f6658b6ed19ad528.js
@@ -1606,8 +1606,8 @@ var wx;
                     exp = opt.parameter;
                     paramObservable = this.domManager.expressionToObservable(exp, ctx);
                 }
-                if (opt.eventNames) {
-                    exp = opt.eventNames;
+                if (opt.events) {
+                    exp = opt.events;
                     eventNames = this.domManager.evaluateExpression(exp, ctx);
                 }
             }
