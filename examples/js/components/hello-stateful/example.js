4ff086d0fc74c86dfde056e363044d2b66357120
Oliver Weichhold <oliver@weichhold.com>
2015-05-23 16:20:54 +0200

    Added documentation links to examples

--- examples/js/components/hello-stateful/example.js
+++ examples/js/components/hello-stateful/example.js
@@ -2,7 +2,7 @@
 define(["require", "exports"], function (require, exports) {
     var ViewModel = (function () {
         function ViewModel() {
-            this.firstName = wx.property(wx.router.current().params.firstName || 'Bart');
+            this.firstName = wx.property(wx.router.current().params.firstName || 'Homer');
             this.lastName = wx.property(wx.router.current().params.lastName || 'Simpson');
             this.fullName = wx.whenAny(this.firstName, this.lastName, function (firstName, lastName) {
                 wx.router.updateCurrentStateParams(function (params) {
